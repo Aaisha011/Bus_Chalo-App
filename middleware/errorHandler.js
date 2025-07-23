95acfb6a2b041e924274587f0e010e33f57f9320
Aaisha Khan <160912968+Aaisha011@users.noreply.github.com>
2025-07-23 10:55:15 +0530

    updated

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,13 +1,13 @@
 const errorHandler = (err,req,res,next) =>{
 
     // console.log("Status code: ",res.statusCode);
-    const resCode = res.statusCode <= 200 ? 500 : res.statusCode;   
+    const resCode = res.statusCode <= 200 ? 500 : res.statusCode;
     res.statusCode = resCode;
 
     res.json({
         msg : err.message,
-        //For on which line this error occured 
-        stack : process.env.NODE_ENV === "production" ? null : err.stack 
+        //For on which line this error occured
+        stack : process.env.NODE_ENV === "production" ? null : err.stack
     })
 }
 
--- server.js
+++ server.js
@@ -40,6 +40,5 @@ app.listen(PORT,() =>{
     console.log(`Server is running on PORT: ${PORT}`.bgMagenta);
 })
 
-
 // Error Handler
 app.use(errorHandler)
